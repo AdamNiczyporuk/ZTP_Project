d8b78622818dd56772147e4874199155ec3126ce
Adam Niczyporuk <60665323+AmoxV2@users.noreply.github.com>
2024-11-26 12:36:29 +0100

    Add Singleton

---
+++ ZTP_Project/Singleton/GlobalnyBudzet.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZTP_Project.Models;
+
+namespace ZTP_Project.Singleton
+{
+    public sealed class GlobalnyBudzet
+    {
+        private static GlobalnyBudzet _instance;
+
+        public Budzet Budzet { get; private set; }
+
+        private GlobalnyBudzet()
+        {
+            Budzet = new Budzet();
+        }
+
+        public static GlobalnyBudzet GetInstance()
+        {
+            if (_instance == null)
+                _instance = new GlobalnyBudzet();
+
+            return _instance;
+        }
+    }
+}
