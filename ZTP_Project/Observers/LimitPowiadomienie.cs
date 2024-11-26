91d56cb587411284ffeeb0afbfdb0aa8c6bf8395
Adam Niczyporuk <60665323+AmoxV2@users.noreply.github.com>
2024-11-26 12:37:37 +0100

    Add Observer

---
+++ ZTP_Project/Observers/LimitPowiadomienie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZTP_Project.Observers
+{
+    public class LimitPowiadomienie : IObserver
+    {
+        public void Powiadom(string wiadomosc)
+        {
+            Console.WriteLine($"[POWIADOMIENIE]: {wiadomosc}");
+        }
+    }
+}
