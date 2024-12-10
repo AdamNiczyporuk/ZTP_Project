9db055a96e74779b1a764c1cb10abe39ebc7434d
Adam Niczyporuk <60665323+AmoxV2@users.noreply.github.com>
2024-12-10 14:35:38 +0100

    Add Interface to Strategy

---
+++ ZTP_Project/Strategy/IReportGeneratorStrategy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZTP_Project.Singleton;
+using Spectre.Console;
+using Spectre.Console.Rendering;
+
+
+namespace ZTP_Project.Strategy
+{
+    public interface IReportGeneratorStrategy
+    {
+        IRenderable GenerateReport(HomeBudget homeBudget, DateTime startDate, DateTime endDate);
+    }
+}
