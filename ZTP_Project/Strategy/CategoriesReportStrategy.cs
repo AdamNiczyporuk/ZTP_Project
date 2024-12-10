f2fbc42b5a0de81d7b0197b735079c98e0e6d27f
FireFly0707 <jakubswietliczny@gmail.com>
2024-12-10 19:05:43 +0100

    implementacja schematu wzorca Strategy

--- ZTP_Project/Strategy/CategoriesReportStrategy.cs
+++ ZTP_Project/Strategy/CategoriesReportStrategy.cs
@@ -1,19 +1,18 @@
-﻿using System;
+﻿using Spectre.Console;
+using Spectre.Console.Rendering;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Spectre.Console;
-using Spectre.Console.Rendering;
 using ZTP_Project.Models.Singleton;
 
 namespace ZTP_Project.Strategy
 {
-    public  class SummaryReportStrategy : IReportGeneratorStrategy
+    public class CategoriesReportStrategy : IReportGeneratorStrategy
     {
         public IRenderable GenerateReport(HomeBudget homeBudget, DateTime startDate, DateTime endDate)
         {
-
             var root = new Tree("Root");
             return root;
         }
