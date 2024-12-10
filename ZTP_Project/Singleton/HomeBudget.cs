45dc3b7ace6a657804cdf6c5083928639f990563
Adam Niczyporuk <60665323+AmoxV2@users.noreply.github.com>
2024-12-10 12:47:45 +0100

    Add Folders To Projects

---
+++ ZTP_Project/Singleton/HomeBudget.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZTP_Project.Singleton
+{
+    public class HomeBudget
+    {
+        private static HomeBudget _instance;
+
+    }
+}
